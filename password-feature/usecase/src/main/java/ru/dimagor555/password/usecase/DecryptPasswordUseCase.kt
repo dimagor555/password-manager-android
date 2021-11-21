a7c74660a7d6462ccefadc4d331b5d66f53c0249
dimagor555 <dimagor555@mail.ru>
2021-11-21 18:41:46 +0300

    refactor(password usecase): change DecryptPassword params to passwordId

--- password-feature/usecase/src/main/java/ru/dimagor555/password/usecase/DecryptPasswordUseCase.kt
+++ password-feature/usecase/src/main/java/ru/dimagor555/password/usecase/DecryptPasswordUseCase.kt
@@ -1,10 +1,14 @@
 package ru.dimagor555.password.usecase
 
 import ru.dimagor555.encryption.domain.Decryptor
-import ru.dimagor555.password.domain.Password
+import ru.dimagor555.password.repository.PasswordRepository
 
 class DecryptPasswordUseCase(
+    private val passwordRepository: PasswordRepository,
     private val decryptor: Decryptor
 ) {
-    operator fun invoke(password: Password) = decryptor.decrypt(password.encryptedPassword)
+    suspend operator fun invoke(passwordId: Int): String {
+        val password = passwordRepository.getById(passwordId)
+        return decryptor.decrypt(password.encryptedPassword)
+    }
 }
