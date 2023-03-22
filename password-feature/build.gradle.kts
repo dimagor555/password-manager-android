4d531c8ad63011ac1162525b027a28f93bc37b3a
dimagor555 <dimagor555@mail.ru>
2023-03-22 17:14:57 +0300

    Fix build.gradle.kts file of password feature

--- password-feature/build.gradle.kts
+++ password-feature/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
     id("com.android.library")
-    id("dev.icerock.mobile.multiplatform-resources")
     id("io.realm.kotlin")
     kotlin("plugin.serialization")
 }
@@ -17,55 +16,42 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api(compose.runtime)
-                api(compose.foundation)
-                api(compose.material)
-                api(compose.preview)
-                api(compose.ui)
-                api(compose.materialIconsExtended)
-
                 implementation(projects.core)
                 implementation(projects.uiCore)
                 implementation(projects.resCore)
                 implementation(projects.validationCore)
-                api(projects.encryptionCore)
+                implementation(projects.encryptionCore)
+
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.preview)
+                implementation(compose.ui)
+                implementation(compose.materialIconsExtended)
 
                 implementation(Libs.MviCompose.core)
                 implementation(Libs.MviCompose.android)
-
                 implementation(Libs.Decompose.decompose)
 
                 implementation(Libs.Koin.core)
                 implementation(Libs.Koin.android)
                 implementation(Libs.Koin.compose)
 
-                api(Libs.KotlinX.datetime)
+                implementation(Libs.KotlinX.datetime)
                 implementation(Libs.KotlinX.serialization)
 
-                implementation(Libs.MokoResources.commonMain)
-
                 implementation(Libs.Realm.base)
-
-                implementation(Libs.KotlinX.reflect)
             }
         }
         val androidMain by getting {
             dependencies {
                 implementation(Libs.MviCompose.core)
                 implementation(Libs.MviCompose.android)
-
-                // TODO remove dead code
-                implementation(Libs.Room.runtime)
-                implementation(Libs.Room.ktx)
-
-                implementation(Libs.MokoResources.androidMain)
             }
         }
         val jvmMain by getting {
             dependencies {
-                api(compose.preview)
-
-                implementation(Libs.MokoResources.jvmMain)
+                implementation(compose.preview)
             }
         }
     }
@@ -88,8 +74,4 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.3.1"
     }
-}
-
-multiplatformResources {
-    multiplatformResourcesPackage = "ru.dimagor555.password"
 }
